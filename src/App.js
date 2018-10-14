ddd2f1d9b7f6349ed2b78e8bed6b38f6b8f7bbc2
crystal_rim <remember2889@gmail.com>
2018-10-14 15:49:12 +0900

    first upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.scss';
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles);
 
 class App extends Component {
   render() {
+    const isBlue = false;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={cx('box', {
+        blue: isBlue
+      })}>
+        <div className={cx('box-inside')} />
       </div>
     );
   }
